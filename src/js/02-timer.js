1bc571006eafc72c1e8f47bd2262ab016153bf22
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-20 14:14:32 +0300

    Try to do task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,25 @@
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+// import "notiflix/dist/notiflix-3.2.5.min.css";
 
+refs = {
+    input: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('[data-start]'),
+    timer: document.querySelector('.timer'),
+    timerDays: document.querySelector('[data-days]'),
+    timerHours: document.querySelector('[data-hours]'),
+    timerMinutes: document.querySelector('[data-minutes]'),
+    timerSeconds: document.querySelector('[data-seconds]')
+
+}
+
+let timerId = null;
+let ms = 0;
+
+// додавання атрибута , що б була не активна кнопка
+refs.startBtn.setAttribute("disabled", "");
 
 const options = {
     enableTime: true,           //   вибір часу (години) 
@@ -11,42 +29,74 @@ const options = {
   
   onClose(selectedDates) {
       console.log(selectedDates[0]);
-
-      if (selectedDates[0] >= options.defaultDate) {
-          refs.startBtn.removeAttribute("disabled");
+    
+      if (selectedDates[0] < options.defaultDate) {
+          
+           Notiflix.Notify.failure('Please choose a date in the future');
           return
       }
-      window.alert("Please choose a date in the future")
-      refs.startBtn.setAttribute("disabled", "");
+      refs.startBtn.removeAttribute("disabled");
+      refs.startBtn.addEventListener('click', () => {
+      Notiflix.Notify.success('Timer started');
+      timerId = setInterval(() => {
+        ms = selectedDates[0] - Date.now();
+        const updateTimerObj = convertMs(ms);
+        
+        updateClockFace(updateTimerObj);
+
+        //   очищення знайчення і зупинка таймера
+        if (ms <= 1000) {
+          clearInterval(timerId);
+          Notiflix.Notify.success('Timer stoped');
+        }
+      }, 1000);
+    });
+
   },
   
 };
 
 
+// підключення календаря + налаштування до нього
+flatpickr(refs.input, options);
 
-flatpickr("#datetime-picker", options);
 
-
-
-refs = {
-    input: document.querySelector('#datetime-picker'),
-    startBtn: document.querySelector('[data-start]'),
-    timer: document.querySelector('.timer'),
-    field: document.querySelector('.field'),
-    value: document.querySelector('.value'),
-    label: document.querySelector('.label'),
-
+// має змінювати знайчення на таймері
+function updateClockFace( { days, hours, minutes, seconds }) {
+    refs.timerDays.textContent = `${days}`;
+    refs.timerHours.textContent = ` ${ hours }`;
+    refs.timerMinutes.textContent = `${ minutes }`;
+    refs.timerSeconds.textContent = `${seconds}`;
 }
 
-// const timePicker = 
+
+// Додає 0 до чила 
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
 
 
-refs.startBtn.addEventListener("click" , () => { })
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
 
 
-// // console.log(refs.input);
+// =========================================================
 
-// function isDateCorrect() {
-//     console.log(selectedDates);
-// }
+
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const refs = { 
+  form: document.querySelector('.form'),
+   delay: document.querySelector('[name=delay]'),
+  step: document.querySelector('[name=step]'),
+  amount: document.querySelector('[name=amount]'),
+  submitBtn: document.querySelector('[type=submit]')
+     
 }
+
+// console.log(refs.submitBtn);
+
+const promise = new Promise((resolve, reject) => { 
+  const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => { 
+    
+  if (shouldResolve) {
+    resolve(`✅ Fulfilled`)
+    
+  }
+  reject (`❌ Rejected`)
+  }, 1500)
+})
+
+console.log(promise);
+
+promise.then(result => {
+  console.log(result);
+}, error => {
+  console.log(error)
+},
+);
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
