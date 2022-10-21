ea03a9be27a79a973d2bf8362cf2f7eaa8ba64bd
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-21 16:33:52 +0300

    update task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
-// import "notiflix/dist/notiflix-3.2.5.min.css";
+import "notiflix/dist/notiflix-3.2.5.min.css";
 
 refs = {
     input: document.querySelector('#datetime-picker'),
