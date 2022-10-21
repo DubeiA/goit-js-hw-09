5199b7775f029d3b7248e5d7f5a4f98eed0e2d52
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-21 16:32:25 +0300

    done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,120 +1,110 @@
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
 const refs = { 
   form: document.querySelector('.form'),
    delay: document.querySelector('[name=delay]'),
   step: document.querySelector('[name=step]'),
   amount: document.querySelector('[name=amount]'),
-  submitBtn: document.querySelector('[type=submit]')
      
 }
 
 
-refs.form.addEventListener('submit' , function (event) {
+refs.form.addEventListener('submit', onSubmit);
+
+
+function onSubmit (event) {
   event.preventDefault();
 
   let {delay, step, amount} = getValue()
 
-  console.log( { delay, step, amount });
-
-  return 
-})
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+  
+  
+}
 
 function getValue() {
   return { 
-    delay: refs.delay.value,
-    step: refs.step.value,
-    amount: refs.amount.value
+   delay: Number(refs.delay.value),
+    step: Number(refs.step.value),
+    amount: Number(refs.amount.value),
  }
 }
 
 
-
-  // ======================================
-  // Проміси
-
-  // const promise = new Promise((resolve, reject) => { 
-  //   const shouldResolve = Math.random() > 0.3;
-  //   setTimeout(() => { 
+function createPromise(position, delay) {
     
-  //   if (shouldResolve) {
-  //     resolve(`Fulfilled`)
-    
-  //   }
-  //   reject (`Rejected`)
-  //   }, 1500)
-  // })
-
-  // console.log(promise);
-
-  // promise.then(onFulldield, onReject);
-
-
-  // function onFulldield(result) {
-  //   console.log(`✅ ${result}`);
-  // }
-
-  // function onReject(error) {
-  //   console.log(`❌ ${error}`);
-  // }
-
-
-
-
-
-
-  // ============================================
-  // рахувалка
-
-
-
-  let repeatCount = 0;
-  const MAX_REPEAT_COUNT = refs.amount.value;
-
-
-  // const repeatPromise = setInterval(() => {
-  //   if (repeatCount === MAX_REPEAT_COUNT) {
-  //     console.log(`done 3 times`);
-  //     clearInterval(repeatPromise)
-  //     return
-  //   }
-
-
-  //     console.log('Start');
-  //     repeatCount += 1;
-  //   }, 1000)
-
-
-
-  if (repeatCount === MAX_REPEAT_COUNT) {
-    console.log(`done 4 times`);
-    return
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+        setTimeout(() => {
+        if (shouldResolve) {
+             resolve({ position, delay });
+         } else {
+        reject({ position, delay })
+        }
+      }, delay)
+    })
+       
   }
 
-  setTimeout(() => {
-    console.log('Start');
-    repeatCount += 1;
-  }, 1000)
 
 
 
-  // ======================================================== 
-  // Матеріал для завдання
 
-  // function createPromise(position, delay) {
-  //   const shouldResolve = Math.random() > 0.3;
-  //   if (shouldResolve) {
-  //     // Fulfill
-  //   } else {
-  //     // Reject
-  //   }
-  // }
 
-  // createPromise(2, 1500)
-  //   .then(({ position, delay }) => {
-  //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  //   })
-  //   .catch(({ position, delay }) => {
-  //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  //   });
 
+// const refs = {
+//   formEl: document.querySelector('.form'),
+//   delayEL: document.querySelector('input[name="delay"]'),
+//   stepEl: document.querySelector('input[name="step"]'),
+//   amountEl: document.querySelector('input[name="amount"]'),
+// };
+
+// refs.formEl.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   let { delay, step, amount } = onGettingData();
+
+//   for (let position = 1; position <= amount; position += 1) {
+//     createPromise(position, delay)
+//       .then(({ position, delay }) => {
+//         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//     delay += step;
+//   }
+// }
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     setTimeout(()=> {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// function onGettingData() {
+//   return {
+//     delay: Number(refs.delayEL.value),
+//     step: Number(refs.stepEl.value),
+//     amount: Number(refs.amountEl.value),
+//   };
+// }
