2816819a18a7145f3b5a8809b8f972383c0bae3b
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-21 14:06:29 +0300

    try to do task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-import Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
 
 const refs = { 
   form: document.querySelector('.form'),
@@ -9,44 +9,112 @@ const refs = {
      
 }
 
-// console.log(refs.submitBtn);
 
-const promise = new Promise((resolve, reject) => { 
-  const shouldResolve = Math.random() > 0.3;
-  setTimeout(() => { 
-    
-  if (shouldResolve) {
-    resolve(`✅ Fulfilled`)
-    
-  }
-  reject (`❌ Rejected`)
-  }, 1500)
+refs.form.addEventListener('submit' , function (event) {
+  event.preventDefault();
+
+  let {delay, step, amount} = getValue()
+
+  console.log( { delay, step, amount });
+
+  return 
 })
 
-console.log(promise);
-
-promise.then(result => {
-  console.log(result);
-}, error => {
-  console.log(error)
-},
-);
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+function getValue() {
+  return { 
+    delay: refs.delay.value,
+    step: refs.step.value,
+    amount: refs.amount.value
+ }
+}
 
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+
+  // ======================================
+  // Проміси
+
+  // const promise = new Promise((resolve, reject) => { 
+  //   const shouldResolve = Math.random() > 0.3;
+  //   setTimeout(() => { 
+    
+  //   if (shouldResolve) {
+  //     resolve(`Fulfilled`)
+    
+  //   }
+  //   reject (`Rejected`)
+  //   }, 1500)
+  // })
+
+  // console.log(promise);
+
+  // promise.then(onFulldield, onReject);
+
+
+  // function onFulldield(result) {
+  //   console.log(`✅ ${result}`);
+  // }
+
+  // function onReject(error) {
+  //   console.log(`❌ ${error}`);
+  // }
+
+
+
+
+
+
+  // ============================================
+  // рахувалка
+
+
+
+  let repeatCount = 0;
+  const MAX_REPEAT_COUNT = refs.amount.value;
+
+
+  // const repeatPromise = setInterval(() => {
+  //   if (repeatCount === MAX_REPEAT_COUNT) {
+  //     console.log(`done 3 times`);
+  //     clearInterval(repeatPromise)
+  //     return
+  //   }
+
+
+  //     console.log('Start');
+  //     repeatCount += 1;
+  //   }, 1000)
+
+
+
+  if (repeatCount === MAX_REPEAT_COUNT) {
+    console.log(`done 4 times`);
+    return
+  }
+
+  setTimeout(() => {
+    console.log('Start');
+    repeatCount += 1;
+  }, 1000)
+
+
+
+  // ======================================================== 
+  // Матеріал для завдання
+
+  // function createPromise(position, delay) {
+  //   const shouldResolve = Math.random() > 0.3;
+  //   if (shouldResolve) {
+  //     // Fulfill
+  //   } else {
+  //     // Reject
+  //   }
+  // }
+
+  // createPromise(2, 1500)
+  //   .then(({ position, delay }) => {
+  //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  //   })
+  //   .catch(({ position, delay }) => {
+  //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  //   });
 
